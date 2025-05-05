ec0a1f8402ffdb11e687b74a9a4695580872fc28
Nguyen Thi Huyen <22120143@student.hcmus.edu.vn>
2025-05-05 14:07:02 +0700

    Config database

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 const port = process.env.SERVER_PORT || 5000;
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
