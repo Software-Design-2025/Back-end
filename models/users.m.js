dace10e37edae008728d402e899f19235ec64644
Nguyen Thi Huyen <22120143@student.hcmus.edu.vn>
2025-06-15 03:38:26 +0700

    Update users schema

--- models/users.m.js
+++ models/users.m.js
@@ -5,15 +5,9 @@ const usersSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    username: {
-        type: String,
-        unique: true
-    },
+    username: String,
     password: String,
-    email: {
-        type: String,
-        unique: true
-    },
+    email: String,
     avatar: {
         type: String,
         default: 'https://i.pinimg.com/736x/2a/53/70/2a5370c752b7f4bd65766f3550afdb5d.jpg'
