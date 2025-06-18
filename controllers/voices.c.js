bfd352189a562d5fe39179ee9e1f4094ecf47f26
Nguyen Thi Huyen <22120143@student.hcmus.edu.vn>
2025-06-18 21:42:05 +0700

    Change check page condition

--- controllers/voices.c.js
+++ controllers/voices.c.js
@@ -8,13 +8,14 @@ module.exports = {
             const data = await VoicesM.find()
                 .skip((page - 1) * per_page)
                 .limit(per_page);
-            const total = await VoicesM.countDocuments();
-            if (page > data.total_pages) {
+            const totalItems = await VoicesM.countDocuments();
+            const totalPages = Math.ceil(totalItems / per_page);
+            if (page > totalPages || page < 1) {
                 return res.status(400).json({ message: 'Invalid page number' });
             }
             return res.status(200).json({
-                total_items: total,
-                total_pages: Math.ceil(total / per_page),
+                total_items: totalItems,
+                total_pages: totalPages,
                 current_page: page,
                 per_page: per_page,
                 voices: data
